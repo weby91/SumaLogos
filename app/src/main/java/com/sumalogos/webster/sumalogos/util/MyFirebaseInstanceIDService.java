8e37d0dcfa027227898111dc811a08e0413efef1
webster <lebronwebz@gmail.com>
2018-01-16 23:39:55 +0700

    Setup firebase

---
+++ app/src/main/java/com/sumalogos/webster/sumalogos/util/MyFirebaseInstanceIDService.java
@@ -0,0 +1,18 @@
+package com.sumalogos.webster.sumalogos.util;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by webster on 16/01/18.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("weby", refreshedToken);
+    }
+}
